e378c4c535bd4c9a2caa05aeac001a4cfd536097
Yevhenii Biloshapka <106679993+YevheniiBiloshapka@users.noreply.github.com>
2024-07-11 23:02:38 +0200

    fix сta slider

--- src/js/cta.js
+++ src/js/cta.js
@@ -1,10 +1,10 @@
-import {Header} from './components/header.js'
-import copyButton from './components/copyButton.js'
+import { Header } from './components/header.js';
+import copyButton from './components/copyButton.js';
 
 function Accordion() {
   const accordionHeader = document.querySelectorAll('[data-action="accordion-header"]');
   accordionHeader.forEach(header => {
-    header.addEventListener('click', function() {
+    header.addEventListener('click', function () {
       const accordionItem = this.closest('[data-action="accordion-item"]');
       accordionHeader.forEach(item => {
         if (item !== this) {
@@ -24,50 +24,61 @@ function Accordion() {
   });
 }
 
-
-var swiper = new Swiper('.cta-swiper', {
-  slidesPerView: 1,
-
-  breakpoints: {
-    1180: {
-      slidesPerView: 1.8,
-      spaceBetween: 48,
-    },
-    1280: {
-      slidesPerView: 2.6,
-      spaceBetween: 24,
-    },
-    1512: {
-      slidesPerView: 3,
-      spaceBetween: 44,
-    },
-    1728: {
-      slidesPerView: 3.16,
-      spaceBetween: 22,
-    },
-  },
-
-  pagination: {
-    el: '.cta-swiper-pagination',
-    clickable: true,
-    renderBullet: function (index, className) {
-      return '<span class="' + className + '"></span>';
-    },
-  },
-});
 document.addEventListener('DOMContentLoaded', function () {
   const swiperWrapper = document.querySelector('.swiper-wrapper');
   const ctaBlock = document.querySelector('.cta-swiper');
 
-    const swiperSlides = swiperWrapper.querySelectorAll('.swiper-slide');
-    if (swiperSlides.length > 3) {
-      ctaBlock.classList.add('is-hidden');
+  var swiper = new Swiper('.cta-swiper', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+
+    breakpoints: {
+      1180: {
+        slidesPerView: 2,
+        spaceBetween: 48,
+      },
+      1280: {
+        slidesPerView: 2.5,
+        spaceBetween: 24,
+      },
+      1512: {
+        slidesPerView: 3,
+        spaceBetween: 44,
+      },
+      1728: {
+        slidesPerView: 3.16,
+        spaceBetween: 22,
+      },
+    },
+
+    pagination: {
+      el: '.cta-swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '"></span>';
+      },
+    },
+
+    on: {
+      init: function () {
+        const swiperSlides = swiperWrapper.querySelectorAll('.swiper-slide');
+        if (swiperSlides.length > 3) {
+          ctaBlock.classList.add('is-hidden');
+        }
+      },
+      slideChange: function () {
+        const swiperSlides = swiperWrapper.querySelectorAll('.swiper-slide');
+        const slidesPerView = swiper.params.slidesPerView;
+        if (swiper.realIndex >= swiperSlides.length - slidesPerView) {
+          ctaBlock.classList.remove('is-hidden');
+        } else {
+          ctaBlock.classList.add('is-hidden');
+        }
+      }
     }
+  });
 
-
-  copyButton()
-  Accordion()
-  Header()
-
+  copyButton();
+  Accordion();
+  Header();
 });
-
