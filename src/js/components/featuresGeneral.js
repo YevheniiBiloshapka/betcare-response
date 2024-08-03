ae51bf8caf8e1a65e9ae417366e7851396e171ed
Yevhenii Biloshapka <106679993+YevheniiBiloshapka@users.noreply.github.com>
2024-08-03 10:34:27 +0200

    fix general features, add animation and new video + mockup

--- src/js/components/featuresGeneral.js
+++ src/js/components/featuresGeneral.js
@@ -3,19 +3,31 @@ export const featuresGeneral = () => {
   const tabs = document.querySelectorAll('.features__tab');
   const delays = [0, 4000, 8000, 12000]; // Задержки в миллисекундах
 
-  const featuresGeneral = () => {
+  let timeoutIds = [];
+
+  const clearExistingTimeouts = () => {
+    timeoutIds.forEach(timeoutId => clearTimeout(timeoutId));
+    timeoutIds = [];
+  };
+
+  const activateTab = (startIndex = 0) => {
+    clearExistingTimeouts();
     tabs.forEach((tab, index) => {
-      setTimeout(() => {
-        tabs.forEach(t => t.classList.remove('active'));
-        tab.classList.add('active');
-      }, delays[index]);
+      if (index >= startIndex) {
+        const delay = delays[index] - (startIndex > 0 ? delays[startIndex] : 0);
+        const timeoutId = setTimeout(() => {
+          tabs.forEach(t => t.classList.remove('active'));
+          tab.classList.add('active');
+        }, delay);
+        timeoutIds.push(timeoutId);
+      }
     });
   };
 
   const observer = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
-        featuresGeneral();
+        activateTab();
         video.play();
 
         observer.unobserve(video);
@@ -26,4 +38,26 @@ export const featuresGeneral = () => {
   });
 
   observer.observe(video);
+
+  tabs.forEach((tab, index) => {
+    tab.addEventListener('click', (event) => {
+      // Добавляем класс пульсации при клике
+      tab.classList.add('pulse');
+
+      setTimeout(() => {
+        tab.classList.remove('pulse');
+      }, 800); // Длительность анимации
+
+      clearExistingTimeouts();
+      video.currentTime = delays[index] / 1000; // Устанавливаем время видео в секундах
+      activateTab(index);
+      video.play();
+    });
+  });
+
+  video.addEventListener('ended', () => {
+    video.currentTime = 0;
+    activateTab();
+    video.play();
+  });
 };
